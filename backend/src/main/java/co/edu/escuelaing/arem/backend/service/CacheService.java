34b9d355d118134b2f473b489c8e51d20d1438f2
Diego Borrero <die.9511@gmail.com>
2018-11-05 03:15:53 -0500

    Only frontend for deploy

--- backend/src/main/java/co/edu/escuelaing/arem/backend/service/CacheService.java
+++
@@ -1,9 +0,0 @@
-package co.edu.escuelaing.arem.backend.service;
-
-import java.util.concurrent.ConcurrentHashMap;
-
-public interface CacheService {
-    void updateCache() throws BackendException;
-    ConcurrentHashMap<String, String> getSupportedCurrencies();
-    ConcurrentHashMap<String, Double> getRates();
-}
